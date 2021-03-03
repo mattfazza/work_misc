3e0ce9b36b8ba3c609b7d79a78dc299173909279
mattfazza <accmattfazza@gmail.com>
2021-03-03 14:23:07 -0800

    scaled down to a way to collapse templates

--- stitcher.py
+++ stitcher.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 def ingestJSON(filePath):
     try:
@@ -12,6 +13,7 @@ def ingestJSON(filePath):
         sys.exit("An error has occurred.")
 
 def stitcher():
+    global stitched_template
     stitched_template = {}
     baseTemplate = ingestJSON("/Users/mfazza/Code/work_misc/templates/modular_base.json") # remove this hardcoded mess
 
@@ -21,24 +23,22 @@ def stitcher():
 
 
 def put_together(current_state, current_base):
-    
+
     if current_state == {}:
         current_state["Project"] = "Project_Name" # remove this hardcoded one, please?
-    
+
     if "Folders" not in current_base:
         return
-
-    if "Folders" not in current_state:
-        current_state["Folders"] = []
-        
-        for folder_index in range(0, len(current_base["Folders"])):
-            folder_object = {"name": current_base["Folders"][folder_index]}
-            current_state["Folders"].append(folder_object)
-
-            put_together(current_state["Folders"][folder_index], current_base["Model"])
-
     
+    level = map(lambda x: {"name": x}, current_base["Folders"])
+    
+    current_state["Folders"] = list(level)
 
+    for folder_index in range(len(current_state["Folders"])):
+        try:
+          put_together(current_state["Folders"][folder_index], current_base["Model"])
+        except KeyError:
+            return
 
 stitcher()
 
